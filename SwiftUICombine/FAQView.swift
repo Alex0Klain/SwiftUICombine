c499e23060fdf201ebc19f77485b7e8b2bd9d816
Alexey Guz <AAGuz@alfabank.ru>
2022-02-19 21:46:04 +0300

    NavigationLink and Link

---
+++ SwiftUICombine/FAQView.swift
@@ -0,0 +1,14 @@
+//
+//  FAQView.swift
+//  SwiftUICombine
+//
+//  Created by Алексей Гузь on 19.02.2022.
+//
+
+import SwiftUI
+
+public struct FAQView: View {
+  public var body: some View {
+    Text("Hello, world!")
+  }
+}
--- SwiftUICombine/MenuRow.swift
+++ SwiftUICombine/MenuRow.swift
@@ -5,6 +5,27 @@
 //  Created by Алексей Гузь on 19.02.2022.
 //
 
-let title: String = "FAQ / Contact"
-let leftIcon: String = "questionmark"
-let rightIcon: String = "chevron.right"
+import SwiftUI
+
+public struct MenuRow: View {
+  public var title: String = "FAQ / Contact"
+  public var leftIcon: String = "questionmark"
+  public var rightIcon: String = "chevron.right"
+  
+  public var body: some View {
+    HStack(spacing: 12.0) {
+      GradientIcon(icon: leftIcon)
+      
+      Text(title)
+        .font(.subheadline)
+        .fontWeight(.semibold)
+      
+      Spacer()
+      
+      Image(systemName: rightIcon)
+        .font(.system(size: 15, weight: .semibold))
+        .opacity(0.3)
+    }
+    .frame(maxWidth: .infinity, alignment: .leading)
+  }
+}
