d601b144d664f9dea4c900c9a87428777d198565
Alexey Guz <AAGuz@alfabank.ru>
2022-02-19 15:00:35 +0300

    Initial Commit

---
+++ SwiftUICombine/SwiftUICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineApp.swift
+//  SwiftUICombine
+//
+//  Created by Алексей Гузь on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
