fa022436d671e483291eb66b60a89e518226d71d
Alexey Guz <AAGuz@alfabank.ru>
2022-03-19 15:57:00 +0300

    Installing Firebase and Firestore

---
+++ SwiftUICombine/Models/Certificate.swift
@@ -0,0 +1,14 @@
+//
+//  Certificate.swift
+//  SwiftUICombine
+//
+//  Created by Алексей Гузь on 19.03.2022.
+//
+
+struct Certificate: Identifiable {
+    var id: String
+    var course: String
+    var date: String
+    var logo: String
+    var instructor: String
+}
